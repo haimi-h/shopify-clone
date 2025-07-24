a906cc5864cbec4082fe3ac6119383d7b0d82886
Haimanot Hailu <haimih292@gmail.com>
2025-07-24 10:41:35 -0900

    path misconfig

--- src/pages/WithdrawalPage.js
+++ src/pages/WithdrawalPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import '../styles/WithdrawalPage.css'; // You'll create this CSS file
+import '../WithdrawalPage.css'; // You'll create this CSS file
 
 export default function WithdrawalPage() {
     const navigate = useNavigate();
