7603f262af3003d033fc62631f17e29418e064b6
Haimanot Hailu <haimih292@gmail.com>
2025-07-27 23:52:22 -0900

    chat path

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,7 +5,7 @@ import {
 import { useEffect, useState, useCallback } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
-import ChatWidget from "../components/ChatWidget"; // Make sure to import ChatWidget
+import ChatWidget from "../pages/ChatWidget"; // Make sure to import ChatWidget
 
 export default function Dashboard() {
   const navigate = useNavigate();
